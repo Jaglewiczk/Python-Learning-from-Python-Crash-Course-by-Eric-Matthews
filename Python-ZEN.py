82882baf2b51c7c3b9253e2f91d3a5100e5ea3d1
Jaglewiczk <108090713+Jaglewiczk@users.noreply.github.com>
2022-07-18 14:42:33 +0200

    exercises

---
+++ Python-ZEN.py
@@ -0,0 +1,6 @@
+print("The Zen of Python, by Tim Peters\nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.")
+print("Complex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.")
+print("Although practicality beats purity.\nErrors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.")
+print("There should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.")
+print("Now is better than never.\nAlthough never is often better than *right* now.\nIf the implementation is hard to explain, it's a bad idea.\nIf the implementation is easy to explain, it may be a good idea.")
+print("Namespaces are one honking great idea -- let's do more of those!")
