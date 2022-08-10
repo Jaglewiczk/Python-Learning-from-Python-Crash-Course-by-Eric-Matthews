3e674f35c02cbce759de4a55b87d28d53f9578ad
Jaglewiczk <108090713+Jaglewiczk@users.noreply.github.com>
2022-08-10 15:13:59 +0200

    For loop exercises - basic

---
+++ working-with-lists-and-for-loop.py
@@ -0,0 +1,7 @@
+magicians = ["Alicja", "Fryderyk", "Justyna", "Karol"]
+for magician in magicians:
+    print(f"{magician.title()}, to była doskonała sztuczka!")
+    print(f"Nie mogę się doczekać Twojej następnej sztuczki, {magician.title()}")
+    
+print("Dziękuję wszystkim!")
+    
