8777e92ae3f899d84468346e4b4ac57849a2df9f
Jaglewiczk <108090713+Jaglewiczk@users.noreply.github.com>
2022-08-11 23:58:13 +0200

    poprawione bledy

--- if-construction.py
+++ if-construction.py
@@ -1,24 +1,23 @@
 banned_users = ["Michalina", "Wiktoria", "Justyna", "Darek", "Halina", "Irena"]
 authorized_users = ["Karol", "Justyna", "Fryderyk", "Paweł"]
 user = input("Podaj swoje imię: ")
-user = user.lower
-if user in banned_users:
+if user.title() in banned_users:
     print(f"{user}, niestety jesteś zablokowany, nie możesz opublikować komentarza")
 else:
     print(f"{user}, opublikuj swój komentarz") 
 
-if user == "Karol":
+if user.lower() == "Karol":
     print("Królu złoty!")
 
-if user not in authorized_users and user in banned_users:
+if user.title() not in authorized_users and user.title() in banned_users:
     print(f"Drogi/a {user}, nie możesz założyć konta")  
-elif user not in authorized_users:
+elif user.title() not in authorized_users:
     print(f"Drogi/a {user}, załóż proszę konto!")
-elif user in authorized_users:
+elif user.title() in authorized_users:
     print(f"Witaj {user}!")
 else:
     print(f"Witaj {user}, załóż konto!")
 
-    
+  
 
 
