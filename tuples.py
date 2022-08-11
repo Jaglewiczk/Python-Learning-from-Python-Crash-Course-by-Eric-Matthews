3c118aa0b2e3317212c6178942d32184513ca1ac
Jaglewiczk <108090713+Jaglewiczk@users.noreply.github.com>
2022-08-11 23:24:43 +0200

    Tuples

---
+++ tuples.py
@@ -0,0 +1,8 @@
+dishes = ("Beef Wellington", "Broth", "Filet Mignon", "Sour Soup", "Pizza")
+for dish in dishes:
+    print(dish)
+# dishes[0] = "Pepper Steak"
+
+dishes = ("Beef Wellington", "Pepper Steak", "Filet Mignon", "Tomahawk Steak", "Pizza")
+for dish in dishes:
+    print(dish)
