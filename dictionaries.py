932f4f1c0875a9d9254efd816bbf89537ea0fa5d
Jaglewiczk <108090713+Jaglewiczk@users.noreply.github.com>
2022-08-12 03:02:05 +0200

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+alien_0 = {"color": "zielony", "points": "5"}
+print(alien_0["color"])
+print(alien_0["points"])
+new_points = alien_0["points"]
+print(f"Zdobyłeś {new_points} punktów!")
+alien_0["x position"] = 34
+alien_0["y position"] = 11
+print(alien_0)
